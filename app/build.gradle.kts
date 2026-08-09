1d344a4492698bbc44870cb540fbbc6677005f97
luinbytes <42706009+luinbytes@users.noreply.github.com>
2026-08-09 22:00:15 +0100

    release: version 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,8 +69,8 @@ fun signingFingerprint(path: String?, password: String?, alias: String?): String
     }.getOrDefault("unavailable")
 }
 
-val appVersionCode = 2
-val appVersionName = "0.2.0"
+val appVersionCode = 3
+val appVersionName = "1.0.0"
 require(Regex("\\d+\\.\\d+\\.\\d+").matches(appVersionName)) {
     "Hermes versionName must be semantic major.minor.patch."
 }
